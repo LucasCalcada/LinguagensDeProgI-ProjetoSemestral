3224a4c797b8acae993f977736e986e3daefd374
Lucas Calçada <lucasmgmcalcada@gmail.com>
2025-06-03 00:53:59 -0300

    feat: add TableView

---
+++ proj-semestral/src/main/java/org/example/screens/table/TableView.java
@@ -0,0 +1,25 @@
+package org.example.screens.table;
+
+import java.util.Arrays;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import org.example.database.entities.DbEntity;
+import org.example.objects.DataObject;
+import org.example.screens.table.builder.DataBuilderRegistry;
+import org.example.screens.table.builder.ITableDataBuilder;
+
+public class TableView {
+  public static <T extends DataObject> JTable buildTable(Class<T> c, DbEntity<T>[] data) {
+    ITableDataBuilder<T> dataBuilder = DataBuilderRegistry.getInstance().getDataBuilder(c);
+
+    DefaultTableModel model = new DefaultTableModel(dataBuilder.dataObjectHeaders(), 0);
+    for (DbEntity<T> d : data) {
+      String[] row = dataBuilder.dataObjectToRow(d);
+      System.out.println(Arrays.toString(row));
+      model.addRow(row);
+    }
+    return new JTable(model);
+  }
+}
